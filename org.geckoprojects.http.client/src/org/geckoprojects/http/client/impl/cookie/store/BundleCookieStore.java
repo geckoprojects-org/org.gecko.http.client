6e05a1bb5fb450d470a12b0d67a0c11bf6bff839
Stefan Bischof <stbischof@bipolis.org>
2021-05-20 11:25:24 +0200

    initial client
    Signed-off-by: Stefan Bischof <stbischof@bipolis.org>

---
+++ org.geckoprojects.http.client/src/org/geckoprojects/http/client/impl/cookie/store/BundleCookieStore.java
@@ -0,0 +1,32 @@
+/*******************************************************************************
+ * Copyright 2021 Data In Motion Consulting GmbH
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ *******************************************************************************/
+
+package org.geckoprojects.http.client.impl.cookie.store;
+
+import java.net.CookieStore;
+
+import org.geckoprojects.http.client.Constants;
+import org.geckoprojects.http.client.CookieStoreName;
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.ServiceScope;
+
+@CookieStoreName("BUNDLE")
+@Component(service = CookieStore.class, scope = ServiceScope.BUNDLE, configurationPid = Constants.PID_HTTP_CLIENT_COOKIE_STORE_BUNDLE)
+public class BundleCookieStore extends AbstractCookieStore {
+
+}
